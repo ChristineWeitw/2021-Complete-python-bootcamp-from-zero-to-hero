8de36e435e61547e524f436eb80ea14ab0bdf21b
Christine Wei <christinewei@Dodos-MacBook-Pro.local>
2021-06-09 17:43:11 +0800

    update codes

---
+++ game.py
@@ -0,0 +1,40 @@
+import random
+
+# Global variables
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
+         'Queen':10, 'King':10, 'Ace':11}
+
+
+# Card - rank,suits,value
+
+class Card:
+    
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[self.rank]
+
+    def __str__(self):
+        return self.rank + of + self.suit
+
+
+# Instanciate a new deck - 52 cards obejects, hold them as a list
+# Shuffle the cards using random()
+# Deal cards from the deck using pop()
+class Deck:
+    
+    def __init__(self):
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit, rank))
+    
+    def __str__(self):
+        
+
+
+
+mylist = ["apple", "banana", "cherry"]
+random.shuffle(mylist)
